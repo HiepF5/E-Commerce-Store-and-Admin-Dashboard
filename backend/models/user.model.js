65914230145ef97fa269c5577729272e14d25a21
Nguyen Cong Hiep <hiepf5.nguyencong.ptit@gmail.com>
2025-04-11 00:20:27 +0700

    fix: correct typo in user model from 'cartTimes' to 'cartItems'

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema(
       required: [true, "Password is required"],
       minLength: [6, "Password must be at least 6 characters long"],
     },
-    cartTimes: [
+    cartItems: [
       {
         quantity: {
           type: Number,
